df13dae645f7be2e40aef8e5b156ef8d03184482
Евгений Боганов <evgenijboganov@MacBook-Pro-Evgenij.local>
2022-04-15 11:39:38 +0300

    some components have been assigned to classes, the cookie and like function has been added

--- src/components/employers-list/employers-list-item/empoyers-list-item.js
+++ src/components/employers-list/employers-list-item/empoyers-list-item.js
@@ -1,11 +1,37 @@
 
-
+import {Component} from "react"
 import "./employees-list-item.css"
 
-const EployerListItem=({name,salary,increase})=>{
-    let classNames="list-group-item d-flex justify-content-between"
+class EployerListItem extends Component{
+    constructor(props){
+        super(props)
+        this.state={
+            increase: false,
+            star:false
+        }
+
+    }
+
+    onIncrease=()=>{
+        this.setState(({increase})=>({
+            increase: !increase
+        }))
+    }
+    onStar=()=>{
+        this.setState(({star})=>({
+            star: !star
+        }))
+    }
+    render(){
+        let {name,salary}=this.props;
+        let {increase}=this.state
+        let {star}=this.state
+        let classNames="list-group-item d-flex justify-content-between"
     if(increase){
         classNames+= " increase"
+    }
+    if(star){
+        classNames+=" like"
     }
   return(
     <li className={classNames}>
@@ -13,7 +39,8 @@ const EployerListItem=({name,salary,increase})=>{
             <input type="text" className="list-group-item-input" defaultValue={salary+"$"}/>
             <div className='d-flex justify-content-center align-items-center'>
                 <button type="button"
-                    className="btn-cookie btn-sm ">
+                    className="btn-cookie btn-sm "
+                    onClick={this.onIncrease}>
                     <i className="fas fa-cookie"></i>
                 </button>
 
@@ -21,10 +48,12 @@ const EployerListItem=({name,salary,increase})=>{
                         className="btn-trash btn-sm ">
                     <i className="fas fa-trash"></i>
                 </button>
-                <i className="fas fa-star"></i>
+                <i className="fas fa-star" onClick={this.onStar}></i>
             </div>
         </li>
   )
+    }
+   
 }
 
 export default EployerListItem
